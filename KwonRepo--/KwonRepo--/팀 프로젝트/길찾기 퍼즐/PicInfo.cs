0e0030ce73e21be4468af5d77321931173548382
mina103125 <133721845+mina103125@users.noreply.github.com>
2024-06-20 20:58:02 +0900

    4방향, 도착 타일에 캐릭터 등장
    .

---
+++ KwonRepo--/KwonRepo--/팀 프로젝트/길찾기 퍼즐/PicInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 길찾기_퍼즐
+{
+    //박스 정보를 보관한는 클래스 정의
+    public class PictureBoxInfo
+    {
+        public int Index { get; set; }
+        public int TileType { get; set; }
+
+        public PictureBoxInfo(int index, int tileType)
+        {
+            Index = index;
+            TileType = tileType;
+        }
+    }
+}
